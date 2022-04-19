c4c63158dfb6501975e5670498efb21094082fdb
Nobrega <renatamarquesdl@gmail.com>
2022-04-19 13:10:08 -0300

    tipos no typeScript

--- aula-19-04/typescript/scripts/tuple.js
+++ aula-19-04/typescript/scripts/tuple.js
@@ -4,3 +4,9 @@ boletim = ['Ana', 9.0, 75];
 console.log(boletim);
 var estoque = ['Arroz', 10];
 console.log(estoque);
+/* criar array de tuplas */
+var participantes = [];
+participantes.push(['Jose', 18]);
+participantes.push(['Maria', 20]);
+participantes.push(['Ana', 21]);
+console.log(participantes);
--- aula-19-04/typescript/tuple.ts
+++ aula-19-04/typescript/tuple.ts
@@ -4,3 +4,19 @@ console.log(boletim);
 
 var estoque: [string, number] = ['Arroz', 10];
 console.log(estoque);
+
+/* criar array de tuplas */
+var participantes:[string, number][] = [];
+
+participantes.push(['Jose', 18])
+participantes.push(['Maria', 20])
+participantes.push(['Ana', 21])
+
+console.log(participantes)
+
+var contato: {
+    id: number,
+    nome: string
+}
+
+contato = {id: 0, nome: 'Maria'}
